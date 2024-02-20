94c93782571c1bd457af6a15ecabb8b92c268c70
Marcelo Romero <143756811+mromeroParkhill@users.noreply.github.com>
2024-02-20 14:45:16 -0600

    Create index.js
    File added for Embedded Webex Apps requirements

---
+++ index.js
@@ -0,0 +1,30 @@
+// Create a new Webex app instance
+var app = new window.Webex.Application();
+
+// Wait for onReady() promise to fulfill before using framework
+app.onReady().then(() => {
+    log("App ready. Instance", app);
+}).catch((errorcode) =>  {
+    log("Error with code: ", Webex.Application.ErrorCodes[errorcode])
+});
+
+// Button click handler to set share URL
+function handleSetShare() {
+    // Replace this with the URL of your shared page
+    var url = "https://www.example.com/shared.html"
+    // "Shared App" is the title of the window or tab that will be created
+    app.setShareUrl(url, "", "Shared App").then(() => {
+        log("Set share URL", url);
+    }).catch((errorcode) => {
+        log("Error: ", Webex.Application.ErrorCodes[errorcode])
+    });
+}
+
+// Utility function to log app messages
+function log(type, data) {
+    var ul = document.getElementById("console");
+    var li = document.createElement("li");
+    var payload = document.createTextNode(`${type}: ${JSON.stringify(data)}`);
+    li.appendChild(payload)
+    ul.prepend(li);
+}
